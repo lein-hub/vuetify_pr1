91b8d77e46fc5b73bab7dc3c779fe998122f5dac
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-08-27 13:50:33 +0900

    form validation

--- vuetify-admin-template/src/views/Forms.vue
+++ vuetify-admin-template/src/views/Forms.vue
@@ -4,40 +4,75 @@
       Form Validation
     </v-card-title>
     <v-card-text>
-      <validation-observer>
+      <validation-observer v-slot="{invalid}">
+        {{ invalid }}
         <v-form>
           <validation-provider
             v-slot="{errors}"
-            name="name"
+            name="Name"
             rules="max:10"
           >
             <v-text-field
               v-model="name"
-              label="name"
+              label="Name"
               :counter="10"
               :error-messages="errors"
             />
           </validation-provider>
-          <validation-provider>
+          <validation-provider
+            v-slot="{errors}"
+            name="PhoneNumber"
+            :rules="{
+              required: true,
+              numeric: true,
+              digits: 11
+            }"
+          >
             <v-text-field
               v-model="phoneNumber"
               label="PhoneNumber"
+              :error-messages="errors"
+              :counter="11"
             />
           </validation-provider>
-          <validation-provider>
+          <validation-provider
+            v-slot="{errors}"
+            name="E-Mail"
+            :rules="{
+              email: true,
+              required: true,
+            }"
+          >
             <v-text-field
               v-model="email"
               label="E-Mail"
+              :error-messages="errors"
             />
           </validation-provider>
-          <validation-provider>
+          <validation-provider
+            v-slot="{errors}"
+            name="Select"
+            :rules="{
+              required
+            }"
+          >
             <v-select
+              v-model="select"
+              :error-messages="errors"
               label="Select"
               :items="items"
             />
           </validation-provider>
-          <validation-provider>
+          <validation-provider
+            v-slot="{errors}"
+            name="CheckBox"
+            :rules="{
+              required
+            }"
+          >
             <v-checkbox
+              v-model="checkbox"
+              :error-messages="errors"
               label="CheckBox"
               value="1"
             />
@@ -45,6 +80,8 @@
           <v-btn
             class="mr-4"
             type="submit"
+            color="primary"
+            :disabled="invalid"
           >
             submit
           </v-btn>
@@ -59,12 +96,37 @@
 
 <script>
 import { extend, ValidationObserver, ValidationProvider} from 'vee-validate'
-extend('max', (value, params) => {
-  const limit = params[0]
-  if (value && value.length > limit) {
-    return `해당 필드는 ${limit}자를 초과할 수 없습니다.`
-  }
-  return true
+import {max, required, numeric, digits, email} from 'vee-validate/dist/rules'
+
+// extend('max', (value, params) => {
+//   const limit = params[0]
+//   if (value && value.length > limit) {
+//     return `해당 필드는 ${limit}자를 초과할 수 없습니다.`
+//   }
+//   return true
+// })
+
+extend('max', {
+  ...max,
+  message: '{_field_}필드는 {length}자를 초과할 수 없습니다.'
+})
+
+extend('required', {
+  ...required,
+  message: '{_field_} 필드는 필수값입니다.'
+})
+extend('numeric', {
+  ...numeric,
+  message: '{_field_} 필드는 숫자로만 구성되어야 합니다.'
+})
+extend('digits', {
+  ...digits,
+  message: '{_field_} 필드는 {length}자리여야 합니다.'
+})
+
+extend('email', {
+  ...email,
+  message: '잘못 입력된 이메일 주소입니다.'
 })
 
 export default {
@@ -80,9 +142,11 @@ export default {
         {text: '아이템2',  value: 2},
         {text: '아이템3',  value: 3}
       ],
-      name: '',
-      phoneNumber: '',
-      email: '',
+      name: null,
+      phoneNumber: null,
+      email: null,
+      select: null,
+      checkbox: null,
     }
   },
 }
